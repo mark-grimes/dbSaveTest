9f705c3234463abb3081873202ae041069609119
Mark Grimes <mark.grimes@rymapt.com>
2017-09-06 11:07:09 +0000

    Add tests using sqlalchemy

---
+++ sqlalchemyBulkInsert/run.sh
@@ -0,0 +1,9 @@
+
+function copyDb {
+
+	echo "Batch size $1"
+	time ./convertDB.py -l mysql://ethoscope:ethoscope@localhost/ethoscope_db -o sqlite:///sqlalchemyBulkInsert_${1}.sqlite -b $1
+}
+
+copyDb 200
+copyDb 2000
